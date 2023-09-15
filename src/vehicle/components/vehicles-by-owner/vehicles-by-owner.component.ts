c0798e077030134d8836bd9db41c938cb3767cbb
Marek Pacześny <mpaczes@tlen.pl>
2023-09-15 21:58:19 +0200

    vehicles by owner - remove error - Expression has changed after iw was checked

--- src/vehicle/components/vehicles-by-owner/vehicles-by-owner.component.ts
+++ src/vehicle/components/vehicles-by-owner/vehicles-by-owner.component.ts
@@ -14,7 +14,7 @@ export class VehiclesByOwnerComponent implements OnInit, OnDestroy {
   mapperOwnerToVehicles = new Map<IOwner, IVehicle[]>();
   ownerVehicles: IVehicle[] | undefined = [];
   ownerVehiclesSubscription!: Subscription;
-  mapKeys: IterableIterator<IOwner> | undefined;
+  // mapKeys: IterableIterator<IOwner> | undefined;
 
   constructor(private vehicleService: VehicleService) {}
 
@@ -33,7 +33,7 @@ export class VehiclesByOwnerComponent implements OnInit, OnDestroy {
         }
       }
 
-      this.setMapKeys();
+      // this.setMapKeys();
 
     });
   }
@@ -43,7 +43,7 @@ export class VehiclesByOwnerComponent implements OnInit, OnDestroy {
       if (this.mapperOwnerToVehicles.has(owner)) {
         this.ownerVehicles = this.mapperOwnerToVehicles.get(owner);
 
-        this.setMapKeys();
+        // this.setMapKeys();
       }
     }
   }
@@ -57,9 +57,14 @@ export class VehiclesByOwnerComponent implements OnInit, OnDestroy {
   }
 
   // Do not bind to a method 'map.keys()' in the template. It will be called every time change detection runs.
-  // 'object.keys()' returns a new object every time is is called and Angular recognizes this as an unexpected change, which leads to error 'Expression has changed after iw was chekced'.
-  setMapKeys() {
-    this.mapKeys = this.mapperOwnerToVehicles.keys();
+  // 'object.keys()' returns a new object every time is is called and Angular recognizes this as an unexpected change, which leads to error 'Expression has changed after iw was checked'.
+  // setMapKeys() {
+  //   this.mapKeys = this.mapperOwnerToVehicles.keys();
+  // }
+
+  // As described above error.
+  getMapKeys() {
+    return Array.from(this.mapperOwnerToVehicles.keys());
   }
 
   ngOnDestroy(): void {
